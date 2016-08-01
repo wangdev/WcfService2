62e25631ea3cdb9bd677423467eb5a96360b8e27
Michael Wang <wangdevelop@gmail.com>
2016-08-01 13:29:31 -0400

    httpbinding client

---
+++ CalculatorServiceWebApp/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CalculatorServiceWebApp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="font-family:Arial">
+        <asp:TextBox ID="textBox1" runat="server"></asp:TextBox>
+        <asp:TextBox ID="textBox2" runat="server"></asp:TextBox>
+        <asp:Button ID="button1" runat="server" Text="Add" OnClick="button1_Click" />
+        <br />
+        <asp:Label ID="label1" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ CalculatorServiceWebApp/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using CalculatorServiceWebApp.CalculatorWcfService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalculatorServiceWebApp {
+    public partial class WebForm1 : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+
+        protected void button1_Click(object sender, EventArgs e) {
+            // We have two endpoints in the web.config. We need to speicy which one we want to use.
+            CalculatorServiceClient proxy = new CalculatorServiceClient("BasicHttpBinding_ICalculatorService");
+            double x = Convert.ToSingle(textBox1.Text.ToString());
+            double y = Convert.ToSingle(textBox2.Text.ToString());
+            double rst = proxy.Add(x, y);
+            label1.Text = rst.ToString();
+        }
+    }
+}
