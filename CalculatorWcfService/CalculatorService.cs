b1de45e625f9fe5476fc04f1c464983392df8e49
Michael Wang <wangdevelop@gmail.com>
2016-08-01 09:45:14 -0400

    create the service

---
+++ CalculatorWcfService/CalculatorService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CalculatorWcfService {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CalculatorService" in both code and config file together.
+    public class CalculatorService : ICalculatorService {
+        public double Add(double x, double y) {
+            return x + y; 
+        }
+    }
+}
---
+++ WcfService2/CalculatorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyServices {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CalculatorService" in both code and config file together.
+    public class CalculatorService : ICalculatorService {
+        public double Add(double x, double y) {
+            return x + y; 
+        }
+
+       
+    }
+}
