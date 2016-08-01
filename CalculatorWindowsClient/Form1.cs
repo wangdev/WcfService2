19397243d630902e106aa898854a961296fb914a
Michael Wang <wangdevelop@gmail.com>
2016-08-01 13:43:26 -0400

    tcp binding client

---
+++ CalculatorWindowsClient/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatorWindowsClient.CalculatorWcfService;
+namespace CalculatorWindowsClient {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            CalculatorServiceClient proxy = new CalculatorServiceClient("NetTcpBinding_ICalculatorService");
+            double x = Convert.ToSingle(textBox1.Text.ToString());
+            double y = Convert.ToSingle(textBox2.Text.ToString());
+            double rst = proxy.Add(x, y);
+            label1.Text = rst.ToString();
+        }
+    }
+}
