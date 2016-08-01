c1b62c0482a5a7f51e00751d5e62409ac38b5f2e
Michael Wang <wangdevelop@gmail.com>
2016-08-01 11:34:40 -0400

    self host in console application

---
+++ CalculatorWcfServiceHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using CalculatorWcfService;
+
+namespace CalculatorWcfServiceHost {
+    class Program {
+        static void Main(string[] args) {
+
+            using (ServiceHost host = new ServiceHost(typeof(CalculatorService))) {
+                host.Open();
+                Console.WriteLine("Host started @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
